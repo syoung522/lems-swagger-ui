cfe423c08156d804c84e5bf1a6a3807f569a1a96
syoung522 <affectionmej@gmail.com>
2024-09-28 18:48:19 +0900

    add swagger.json to dist folder

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://github.com/syoung522/lems-swagger-ui/tree/master/dist/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
